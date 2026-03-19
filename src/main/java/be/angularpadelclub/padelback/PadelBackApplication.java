b183ce547fab83247a7442c678e191784fd5a34b
vince.h <v.harmegnies@gmail.com>
2026-03-19 18:51:40 +0100

    first push

---
+++ src/main/java/be/angularpadelclub/padelback/PadelBackApplication.java
@@ -0,0 +1,13 @@
+package be.angularpadelclub.padelback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadelBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/angularpadelclub/padelback/PadelBackApplicationTests.java
@@ -0,0 +1,13 @@
+package be.angularpadelclub.padelback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
