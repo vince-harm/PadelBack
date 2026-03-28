323428b930a566b3d272082e985c814cc8a08528
vince.h <v.harmegnies@gmail.com>
2026-03-28 16:39:51 +0100

    change WebConfig

--- src/main/java/be/angularpadelclub/padelback/config/WebConfig.java
+++ src/main/java/be/angularpadelclub/padelback/config/WebConfig.java
@@ -2,16 +2,16 @@ package be.angularpadelclub.padelback.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @Configuration
 public class WebConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
@@ -21,4 +21,5 @@ public class WebConfig {
             }
         };
     }
+
 }
