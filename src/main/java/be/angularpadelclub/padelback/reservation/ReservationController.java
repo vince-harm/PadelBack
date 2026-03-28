5f6436d8b617dc0a3272208ec35924a9cbd6cc59
vince.h <v.harmegnies@gmail.com>
2026-03-28 12:47:03 +0100

    ReservationController

--- src/main/java/be/angularpadelclub/padelback/reservation/ReservationController.java
+++ src/main/java/be/angularpadelclub/padelback/reservation/ReservationController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/reservations")
@@ -14,10 +17,34 @@ import java.util.List;
 
 public class ReservationController {
 
+    private static final List<reservation> RESERVATIONS = new ArrayList<>();
+    static {
+        RESERVATIONS.add(new reservation(
+                UUID.randomUUID(),
+                "Court 1",
+                LocalDate.of(2026, 3, 28),
+                "10:00",
+                "Vincent"
+        ));
+        RESERVATIONS.add(new reservation(
+                UUID.randomUUID(),
+                "Court 2",
+                LocalDate.of(2026, 3, 28),
+                "14:30",
+                "Christelle"
+        ));
+    }
+
+    @GetMapping(produces = "application/json")
+    public List<reservation> getAll() {
+        return RESERVATIONS;
+    }
+
     @GetMapping
     public List<reservation> getReservations() {
         return List.of();
     }
+
     @PostMapping
     public reservation createReservation(@RequestBody reservation reservation) {
         return reservation;
